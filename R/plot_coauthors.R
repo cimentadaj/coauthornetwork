ae96fc43b756492a874dfd6854024501ba879031
cimentadaj <cimentadaj@gmail.com>
2018-06-19 17:56:04 +0200

    Adds optional argument for size of labels

--- R/plot_coauthors.R
+++ R/plot_coauthors.R
@@ -1,6 +1,7 @@
 #' Plot a network of coauthors
 #'
 #' @param network A data frame given by \code{grab_network}
+#' @param size_labels Size of the label names
 #'
 #' @return a \code{ggplot2} object but prints a plot as a side effect.
 #' @export
@@ -11,7 +12,7 @@
 #' final_network <- grab_network('citations?user=amYIKXQAAAAJ&hl=en')
 #' plot_coauthors(final_network)
 #' }
-plot_coauthors <- function(network) {
+plot_coauthors <- function(network, size_labels = 5) {
   graph <-
     network[c("author", "coauthors")] %>%
     tidygraph::as_tbl_graph() %>%
@@ -21,7 +22,7 @@ plot_coauthors <- function(network) {
     ggraph::ggraph(layout = 'kk') +
     ggraph::geom_edge_link(ggplot2::aes_string(alpha = '..index..', color = as.character('from')), alpha = 1/3, show.legend = FALSE) +
     ggraph::geom_node_point(ggplot2::aes_string(size = 'closeness'), alpha = 1/2, show.legend = FALSE) +
-    ggraph::geom_node_text(ggplot2::aes_string(label = 'name'), size = 5, repel = TRUE, check_overlap = TRUE) +
+    ggraph::geom_node_text(ggplot2::aes_string(label = 'name'), size = size_labels, repel = TRUE, check_overlap = TRUE) +
     ggplot2::labs(title = glue::glue("Network of coauthorship of {network$author[1]}")) +
     ggraph::theme_graph(title_size = 16)
 }
