0a7a79f76df6cfbf9d36c0b2eb661d5b2a2c30d7
cimentadaj <cimentadaj@gmail.com>
2018-06-20 08:52:58 +0200

    Removes some dependencies and adds info on DESCRIPTION

--- R/grab_network.R
+++ R/grab_network.R
@@ -1,13 +1,12 @@
 #' Grab a network of coauthors from Google Scholar profile
 #'
-#' @param scholar_id The endline of a google scholar profile. For example, 'citations?user=amYIKXQAAAAJ&hl=en' comes
+#' @param scholar_id The endline of a Google Scholar profile. For example, 'citations?user=amYIKXQAAAAJ&hl=en' comes
 #' from the profile https://scholar.google.com/citations?user=amYIKXQAAAAJ&hl=en
 #' @param n_coauthors Number of coauthors to explore. This number should usually be between 1 and 10 as
 #' choosing many coauthors can make the network graph too messy.
 #' @param n_deep The number of degrees that you want to go down the network. When \code{n_deep} is equal to \code{1}
 #' then \code{grab_network} will only grab the coauthors of Joe and Mary, so Joe -- > Mary --> All coauthors. This can get
-#' out of control very quickly as the network will grow exponentially. I suggest the reader to not use a number more
-#' than \code{2} and the preferred number would be \code{1}, the default.
+#' out of control very quickly if \code{n_deep} is set to \code{2} or above. The preferred number is \code{1}, the default.
 #'
 #' @return A \code{tibble} with the all authors and coauthors.
 #' @export
@@ -15,6 +14,8 @@
 #' @examples
 #'
 #' \dontrun{
+#'
+#' library(coauthornetwork)
 #'
 #' final_network <- grab_network('citations?user=amYIKXQAAAAJ&hl=en')
 #' plot_coauthors(final_network)
@@ -45,18 +46,23 @@ grab_network <- function(scholar_id, n_coauthors = 5, n_deep = 1) {
 get_coauthors <- function(scholar_id, n_coauthors) {
 
   if (scholar_id == "") {
-    return(tibble::tibble(author = character(),
+    return(
+      tidygraph::as_tibble(
+        data.frame(author = character(),
                   href = character(),
                   coauthors = character(),
-                  coauthors_href = character()))
+                  coauthors_href = character())
+        )
+      )
   }
 
-  resp <- httr::GET(glue::glue("https://scholar.google.es/{scholar_id}"))
+  gs_id <- paste0("https://scholar.google.es/", scholar_id)
+  resp <- httr::GET(gs_id)
 
   try <- 1
   while (httr::status_code(resp) != 200 & try <= 5) {
     Sys.sleep(1)
-    resp <- httr::GET(glue::glue("https://scholar.google.es/{scholar_id}"))
+    resp <- httr::GET(gs_id)
     try <- try + 1
   }
 
@@ -89,11 +95,13 @@ get_coauthors <- function(scholar_id, n_coauthors) {
     coauthor_href <- ""
   }
 
-  tibble::tibble(
+  tidygraph::as_tibble(
+    data.frame(
     author = author_name,
     href = scholar_id,
     coauthors = coauthors,
     coauthors_href = coauthor_href
+    )
   )
 }
 
--- R/plot_coauthors.R
+++ R/plot_coauthors.R
@@ -9,20 +9,22 @@
 #' @examples
 #'
 #' \dontrun{
+#' library(coauthornetwork)
+#'
 #' final_network <- grab_network('citations?user=amYIKXQAAAAJ&hl=en')
 #' plot_coauthors(final_network)
 #' }
 plot_coauthors <- function(network, size_labels = 5) {
   graph <-
-    network[c("author", "coauthors")] %>%
-    tidygraph::as_tbl_graph() %>%
-    tidygraph::mutate(closeness = suppressWarnings(tidygraph::centrality_closeness()))
+    tidygraph::mutate(
+      tidygraph::as_tbl_graph(network[c("author", "coauthors")]),
+      closeness = suppressWarnings(tidygraph::centrality_closeness())
+    )
 
-  graph %>%
-    ggraph::ggraph(layout = 'kk') +
+  ggraph::ggraph(graph, layout = 'kk') +
     ggraph::geom_edge_link(ggplot2::aes_string(alpha = '..index..', color = as.character('from')), alpha = 1/3, show.legend = FALSE) +
     ggraph::geom_node_point(ggplot2::aes_string(size = 'closeness'), alpha = 1/2, show.legend = FALSE) +
     ggraph::geom_node_text(ggplot2::aes_string(label = 'name'), size = size_labels, repel = TRUE, check_overlap = TRUE) +
-    ggplot2::labs(title = glue::glue("Network of coauthorship of {network$author[1]}")) +
+    ggplot2::labs(title = paste0("Network of coauthorship of", network$author[1])) +
     ggraph::theme_graph(title_size = 16)
 }
