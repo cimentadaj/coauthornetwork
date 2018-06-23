ab13ee5027c1cd5d2049aff25b46b4e84c0c6834
Max Czapanskiy <maxczapanskiy@gmail.com>
2018-06-22 18:33:30 -0700

    Fix issue 7
    Add a space in plot title between "Network of coauthorship of" and the scholar name

--- R/plot_coauthors.R
+++ R/plot_coauthors.R
@@ -25,6 +25,6 @@ plot_coauthors <- function(network, size_labels = 5) {
     ggraph::geom_edge_link(ggplot2::aes_string(alpha = '..index..', color = as.character('from')), alpha = 1/3, show.legend = FALSE) +
     ggraph::geom_node_point(ggplot2::aes_string(size = 'closeness'), alpha = 1/2, show.legend = FALSE) +
     ggraph::geom_node_text(ggplot2::aes_string(label = 'name'), size = size_labels, repel = TRUE, check_overlap = TRUE) +
-    ggplot2::labs(title = paste0("Network of coauthorship of", network$author[1])) +
+    ggplot2::labs(title = paste0("Network of coauthorship of ", network$author[1])) +
     ggraph::theme_graph(title_size = 16)
 }
